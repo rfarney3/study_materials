f704ceabb730572302ca9ea80f96fb23526d8834
rfarney3 <farneyr@yahoo.com>
2018-09-13 13:25:40 -0700

    palindrome

---
+++ palindrome/index.js
@@ -0,0 +1,11 @@
+// const palindrome = (string) => {
+//   return string.toLowerCase() === string.split("").reverse().join("").toLowerCase()
+// }
+
+// const palindrome = (string) => {
+//   let arr = string.split("")
+//
+//   return arr.every((letter, i) => {
+//     return letter === arr[arr.length - i - 1]
+//   })
+// }
