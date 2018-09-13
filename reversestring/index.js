f225998d0c9a6c50b20a1c6ae4f272b2a9cf9c32
rfarney3 <farneyr@yahoo.com>
2018-09-13 13:02:04 -0700

    reverse string

---
+++ reversestring/index.js
@@ -0,0 +1,17 @@
+// const reverseString = (string) => {
+//   return string.split("").reverse().join("")
+// }
+
+// const reverseString = (string) => {
+//   let str = ""
+//   for (letter of string) {
+//     str = letter + str
+//   }
+//   return str
+// }
+
+// const reverseString = (string) => {
+//   let arr = string.split("")
+//
+//   return arr.reduce((rev, char) => char + rev, "")
+// }
